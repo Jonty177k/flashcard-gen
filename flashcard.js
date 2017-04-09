cf779dc0472243d33ef0681a520439f6e3d70c6c
Jonty177k <mreid17k@gmail.com>
2017-04-09 05:05:15 -0400

    Add files via upload

--- flashcard.js
+++ flashcard.js
@@ -1,27 +1,85 @@
 //npm install inquirer
 var inquirer = require("inquirer");
 
-function BasicCard (font, back) {
+function BasicCard (front, back) {
 	this.front = front;
 	this.back = back;
 }
 
-function ClozeCard (cloze, partial, text) {
+function ClozeCard (cloze, partial) {
 	this.cloze = cloze;	
-	this.partial = "_________" + partial;
-	this.text = function(partial, cloze) {
-		var cardText = partial + " " + cloze;
-		return cardText;
-	};
+	this.partial = partial;
 }
 
+	//card index
+	var m = 0;
+
+	//name bank
+	var bank = [];
+	var card_;
+
+	for (var i=0; i < 50; i++) {
+		bank[i] = card_+i;
+	};
+
+console.log("\n");
 console.log("--------------------------");
 console.log("----Matthews Card App!----");
 console.log("--------------------------");
+console.log("\n");
 
 //var cardArray = [];
 
 function startSetup() {
+
+	function basicSetUp() {
+		//console.log("hello basic");
+		inquirer.prompt([
+		{
+			type: "input",
+			name: "front",
+			message: "Write question that will display on front of card:"
+		},{
+			type: "input",
+			name: "back",
+			message: "Write answer that will display on back of card:"
+		}]).then(function(answers){
+			m++;
+			var bank[m] = new BasicCard(answers.front, answers.back);
+			//console.log(cardName);
+			console.log("\n");
+			console.log("You created a new card called: " + bank[m]);
+			console.log("\n");
+			startSetup();
+		});
+	}
+
+	function clozeSetup() {
+		inquirer.prompt([
+	{
+		name: "cloze",
+		type: "input",
+		message: "What's your cloze?"
+	}, {
+		name: "partial",
+		type: "input",
+		messagge: "What's your partial?"
+	}]).then(function(answers) {
+		m++ 
+		var bank[m] = new ClozeCard(answers.cloze, answers.partial);
+		bank[m].text = answers.cloze + " " + answers.partial;
+	});
+	function viewCards() {
+		inquirer.prompt([
+		{
+			name: "input",
+			message: "Call your card:"
+		}]).then(function(answers) {
+			var input = answers.input;
+			console.log(eval(input));
+		})
+	}
+
 	inquirer.prompt([
 	{
 		name: "choose",
@@ -33,6 +91,7 @@ function startSetup() {
 			"Use Cards"
 		]
 	}]).then(function(answers) {
+
 		if (answers.choose === "Create Basic Card") {
 			basicSetUp();
 		}
@@ -40,40 +99,20 @@ function startSetup() {
 			clozeSetup();
 		}
 		if (answers.choose === "Use Cards") {
-			useCards();
+			console.log("\n");
+			console.log("    example of card call: 'card_#.front' OR 'card_#.cloze'");
+			console.log("\n");
+			viewCards();
 		}
 	});
 }
 
-function basicSetUp() {
-	//console.log("hello basic");
-	inquirer.prompt([
-	{
-		type: "input",
-		name: "card",
-		message: "Name your card:"
-	},{
-		type: "input",
-		name: "front",
-		message: "Write question that will display on front of card:"
-	},{
-		type: "input",
-		name: "back",
-		message: "Write answer that will display on back of card:"
-	}]).then(function(answers){
-
-		var str(answers.card) = new BasicCard(answers.front, answers.back);
-		console.log("You created a new card!");
-	});
-	startSetup();
-}
-
-function clozeSetup() {
-	//console.log("hello cloze");
-}
-
-function viewCards() {
-
-}
-
 startSetup();
+
+
+
+
+
+
+
+
